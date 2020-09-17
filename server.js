0963fc1e845a2f6800ba706681de81a3fdaab41f
Aniket190 <aniketkumar190@gmail.com>
2020-09-17 15:40:26 +0530

    initial author rouutes

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const express = require('express')
 const app= express();
 const path=require('path');
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser=require('body-parser');
 
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 
 
 app.set('view engine','ejs');
@@ -21,7 +23,7 @@ app.use(expressLayouts);
 
 
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 const mongoose=require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true,
@@ -35,6 +37,7 @@ db.on('error',error=>console.error(error))
 db.once('open',()=>console.log('Mongoose is connected'));
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 
 
